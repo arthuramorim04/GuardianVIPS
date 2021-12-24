42674b177cd28a7ac6419670552eb43c5444890f
arthuramorim <arthurpas67@gmail.com>
2021-12-23 22:09:58 -0300

    adicionado arquivos iniciais

---
+++ src/main/java/GuardianVIPS.java
@@ -0,0 +1,14 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GuardianVIPS extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
